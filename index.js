0ef86b1a6e7f801bdf900e4164297cd37f7689ab
mjk <keesan@gmail.com>
2017-12-11 16:16:35 -0500

    Including null properties

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ module.exports = DotenvPlugin;
 function DotenvPlugin(options) {
   // Load into process.env, and keep track of all the
   // keys we care about for webpack serialization. 
-  this.config = dotenv.load(options);
+  this.config = dotenv.load(options) || {};
 }
 
 DotenvPlugin.prototype.apply = function(compiler) {
   const definitions = {};
 
-  Object.keys(this.definitions).forEach((key) => {
-    if (this.config[key]) {
+  Object.keys(process.env).forEach((key) => {
+    if (this.config.hasOwnProperty(key)) {
       definitions[key] = JSON.stringify(process.env[key]);
     }
   });
