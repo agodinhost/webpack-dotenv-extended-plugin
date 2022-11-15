afd7991f0ea442dbc269737b6ddbb9a45c0ce6eb
Woody <1218459+agodinhost@users.noreply.github.com>
2022-11-15 19:02:56 -0300

    Update index.js
    add the webpack expanded plugin call

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv-extended');
+const dotenvExpand = require('dotenv-expand')
 const fs = require('fs');
 const EnvironmentPlugin = require('webpack').EnvironmentPlugin;
-
-module.exports = DotenvPlugin;
 
 function DotenvPlugin(options) {
   this.options = options || {};
@@ -10,6 +9,14 @@ function DotenvPlugin(options) {
   // Load into process.env, and keep track of all the
   // keys we care about for webpack serialization. 
   this.config = dotenv.load(options) || {};
+
+  if ( options.expand ) {
+    const env = dotenv.config();
+    dotenvExpand.expand({
+      ignoreProcessEnv: false,
+      parsed: this.config
+    });
+  }
 }
 
 DotenvPlugin.prototype.apply = function(compiler) {
@@ -25,5 +32,10 @@ DotenvPlugin.prototype.apply = function(compiler) {
     console.log('Applying dotenv configuration', JSON.stringify(definitions, null, 2));
   }
 
-  compiler.apply(new EnvironmentPlugin(Object.keys(this.config)));
+  //compiler.apply(new EnvironmentPlugin(Object.keys(this.config)));
+
+  new EnvironmentPlugin(Object.keys(this.config))
+    .apply(compiler);  
 };
+
+module.exports = DotenvPlugin;
